554f0ebfefdd2da3113953b5b5af971f5be0e48e
paige11 <ellis.paige.v@gmail.com>
2016-08-09 14:13:28 -0400

    changed Cli#read_now to #from_summary and reordered options

--- new-york-reader/lib/cli.rb
+++ new-york-reader/lib/cli.rb
@@ -1,3 +1,4 @@
+
 require_relative '../config/environment'
 
 class Cli
@@ -30,62 +31,87 @@ class Cli
 
   def choose_article
     puts "\n"
-    puts "If you would like to read an article, enter the article number."
-    puts "You can also enter 'summaries' to look at summaries of the articles before you commit to reading."
+    puts "Enter the number of an article you would like to explore."
     puts "Type 'exit' to exit the program."
     gets.strip
-  end # for launch AND read in terminal
-
-  def summaries_or_read
-    @input = choose_article
-    if @input == "summaries"
-      sum_one_or_all
-    elsif @input.to_i.between?(1,10)
-      read_or_launch
-    elsif @input != "exit"
-      invalid
-      summaries_or_read
-    end
   end 
 
-  def read_or_launch
+  def menu
     puts "\n"
-    puts "Enter 'read' if you would like to read this article here."
-    puts "Enter 'launch' if you would like to launch this article in your browser."
-    answer = gets.strip
-    if answer == "read"
-      read_article
-    elsif answer == "launch"
-      launch_article
-    else 
-      invalid
-      read_or_launch
-    end
-  end
-
-  # def launch_article
-  #   index = @input.to_i - 1
-  #   Launchy.open("#{Article.all[index].article_url}")
-  #   list_or_exit
-  # end
-
-  def summary_prompt
-    puts "\n"
-    puts "Enter an article number to read a summary of it, or enter 'all' to display summaries of all articles."
+    puts "-Menu-"
+    puts "Enter 's' if you would like to see a summary of this article before committing to read it."
+    puts "Enter 'r' to read the article here."
+    puts "Enter 'l' to launch the article in your browser."
+    puts "Type 'exit' to exit the program."
     gets.strip
   end
 
-  def sum_one_or_all
-    @input = summary_prompt
-    if @input == "all"
-      summarize_all
-    elsif @input.to_i.between?(1,10)
+  def choose_menu_option
+    @input = choose_article # way to check if valid
+    choice = menu
+    if choice == 's'
       read_summary
-    else
+    elsif choice == 'r'
+      read_article
+    elsif choice == 'l'
+      launch_article
+    elsif choice != 'exit'
       invalid
-      sum_one_or_all
+      choose_option
     end
   end
+
+
+  # def summaries_or_read
+  #   @input = choose_article
+  #   if @input == "summaries"
+  #     sum_one_or_all
+  #   elsif @input.to_i.between?(1,10)
+  #     read_or_launch
+  #   elsif @input != "exit"
+  #     invalid
+  #     summaries_or_read
+  #   end
+  # end 
+
+  # def read_or_launch
+  #   puts "\n"
+  #   puts "Enter 'read' if you would like to read this article here."
+  #   puts "Enter 'launch' if you would like to launch this article in your browser."
+  #   answer = gets.strip
+  #   if answer == "read"
+  #     read_article
+  #   elsif answer == "launch"
+  #     launch_article
+  #   else 
+  #     invalid
+  #     read_or_launch
+  #   end
+  # end
+
+  def launch_article
+    index = @input.to_i - 1
+    Launchy.open("#{Article.all[index].article_url}")
+    list_or_exit
+  end
+
+  # def summary_prompt
+  #   puts "\n"
+  #   puts "Enter an article number to read a summary of it, or enter 'all' to display summaries of all articles."
+  #   gets.strip
+  # end
+
+  # def sum_one_or_all
+  #   @input = summary_prompt
+  #   if @input == "all"
+  #     summarize_all
+  #   elsif @input.to_i.between?(1,10)
+  #     read_summary
+  #   else
+  #     invalid
+  #     sum_one_or_all
+  #   end
+  # end
 
   def read_summary
     index = @input.to_i - 1
@@ -94,25 +120,26 @@ class Cli
     puts Article.all[index].author
     puts "Published: " + Article.all[index].time
     puts Article.all[index].summary
-    read_now
+    from_summary
   end 
 
-  def read_article
-    index = @input.to_i - 1
-    Article.formatted_body(index)
-    list_or_exit
-  end
+  # def read_article
+  #   index = @input.to_i - 1
+  #   article = Article.all[index]
+  #   article.format_body
+  #   list_or_exit
+  # end
 
-  def summarize_all
-    Article.all.each_with_index do |article, i|
-      puts "\n"
-      puts "#{i + 1}. " + article.title
-      puts article.author
-      puts "Published: " + article.time
-      puts article.summary
-    end
-    read_or_exit
-  end
+  # def summarize_all
+  #   Article.all.each_with_index do |article, i|
+  #     puts "\n"
+  #     puts "#{i + 1}. " + article.title
+  #     puts article.author
+  #     puts "Published: " + article.time
+  #     puts article.summary
+  #   end
+  #   read_or_exit
+  # end
 
   def list_or_exit
     puts "\n"
@@ -127,37 +154,41 @@ class Cli
     end
   end
 
-  def read_or_exit
-    puts "\n"
-    puts "Enter the number of an article to read it."
-    puts "Type 'exit' to exit the program."
-    @input = gets.strip
-    if @input.to_i.between?(1,10)
-      read_or_launch
-    elsif @input != "exit"
-      invalid
-      read_or_exit
-    end
-  end
+  # def read_or_exit
+  #   puts "\n"
+  #   puts "Enter the number of an article to read it."
+  #   puts "Type 'exit' to exit the program."
+  #   @input = gets.strip
+  #   if @input.to_i.between?(1,10)
+  #     read_or_launch
+  #   elsif @input != "exit"
+  #     invalid
+  #     read_or_exit
+  #   end
+  # end
 
-  def read_now
+  def from_summary
     puts "\n"
-    puts "Would you like to read this article, y/n?"
+    puts "If you would like to read this article, enter 'r'."
+    puts "If you would like to launch this article in your browser, enter 'l'."
+    puts "To display summaries of all articles, enter 'all'."
+
     answer = gets.strip
-    if answer == "y"
-      read_or_launch
-    elsif answer == "n"
-      numbered_list
-      summaries_or_read
+    if answer == "r"
+      read_article
+    elsif answer == "l"
+      launch_article
+    elsif answer == "all"
+      
     else
       invalid
-      read_now
+      from_summary
     end
   end
 
   def run_nyr
     numbered_list
-    summaries_or_read
+    choose_menu_option
   end
 
   def invalid
@@ -166,6 +197,3 @@ class Cli
   end
 
 end
-
-
-
